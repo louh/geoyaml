42282b90a124702c6436228c59cf207aa736cc6c
Lou Huang <lou@louhuang.com>
2021-05-19 21:09:58 -0700

    feat: add compiled and minified bundles

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## v0.3.0
+
+- Add compiled and minimized bundles
+
 ## v0.1.0 - v0.2.0
 
 - Exists
---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+const { nodeResolve } = require('@rollup/plugin-node-resolve')
+const commonjs = require('@rollup/plugin-commonjs')
+const nodePolyfills = require('rollup-plugin-node-polyfills')
+const { terser } = require('rollup-plugin-terser')
+
+const plugins = [ nodePolyfills(), nodeResolve(), commonjs() ]
+const umd_out_base = { format: 'umd', name: 'geoyaml', exports: 'named' }
+
+module.exports = [
+  // es5
+  {
+    input: 'lib/index.js',
+    output: [
+      { ...umd_out_base, file: 'dist/geoyaml.js' },
+      { ...umd_out_base, file: 'dist/geoyaml.min.js', plugins: [ terser() ] }
+    ],
+    plugins
+  },
+  // esm
+  {
+    input: 'lib/index.js',
+    output: [
+      { format: 'esm', file: 'dist/geoyaml.mjs' },
+    ],
+    plugins
+  }
+]
